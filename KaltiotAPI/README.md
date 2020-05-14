7f98282d6ee9ad8f66a9a24c1e0099492185734e
Aapo Rista <aapo.gitpub@rista.net>
2020-05-14 17:43:06 +0300

    Completely rewrite application logic

--- KaltiotAPI/README.md
+++ KaltiotAPI/README.md
@@ -3,66 +3,85 @@
 Python script which makes a request to Kaltiot API and 
 returns a Pandas DataFrame containing all the data.
 
-Currently result data is resampled to make timestamps match
-and avoid NaN cells in the DataFrame.
-
 # Usage
+
+Install `fvhdms` library first.
 
 See tag-list_example.txt how you should list your
 tag ids and names.
 
-You need a Kaltiot API key to do this:
+Due the implementation of Kaltiot API backend, 
+requests may get `HTTP 206 Partial Content` response.
+This script handels this situation by making the request
+again and again after some time until the backend 
+process is ready to serve data.  
 
 ## Request
 
+You'll need a Kaltiot API key to do this.
+
+To get all the data from beginning of the time in 
+7 day chunks (maximum is 14 days)
+and save the result into a CSV file,
+you can use this command:
+
 ```
 python kaltiot.py \
-    --starttime 20200425T080000EET \ 
-    --endtime 20200426T130000EET \
-    -r 60min -n 09,10,11,12,13 -t tag-list.txt \
-    -A "abcdeyourapikeyhere7890" \
-    -m motion_detected -l INFO
-``` 
+    --starttime 20200424T000000EET \
+    --endtime now \
+    --tagfile tag-list.txt \
+    --apikey "abcdeyourapikeyhere7890" \ 
+    --baseurl https://beacontracker.kalt.io/api/history/sensor/
+    --measurement all \ 
+    --maxperiod 7d \
+    --log INFO \
+    --outfile 20200424T000000EEST-$(date +%Y%m%dT%H%M%S%Z).csv
+```
+## Result
+
+Kaltion script support saving results into CSV/Excel file
+or into InfluxDB.
+
+### DataFrame
+
+```
+                          name        dev-id  collision_x  collision_y  collision_z   humidity  motion_detected     pressure  temperature
+time                                                                                                                                     
+2020-04-23 21:00:03+00:00   20  d7356479a2d9          0.0          0.0          0.0  42.450001              0.0  1009.799988         7.12
+2020-04-23 21:00:06+00:00   25  dc565c713989          0.0          0.0          0.0  45.150002              0.0  1010.000000         6.67
+2020-04-23 21:00:09+00:00   22  f90454752e49          0.0          0.0          0.0  45.730000              0.0  1009.599976         6.97
+2020-04-23 21:00:37+00:00   07  d60c73a9f7f1          0.0          0.0          0.0  45.410000              0.0  1007.500000         4.31
+2020-04-23 21:00:38+00:00   04  f231f773d177          0.0          0.0          0.0  43.889999              0.0  1006.799988         4.10
+...                        ...           ...          ...          ...          ...        ...              ...          ...          ...
+2020-05-14 14:24:06+00:00   24  cfcee9a17f16          0.0          0.0          0.0  51.060001              0.0  1010.500000         8.40
+2020-05-14 14:25:06+00:00   24  cfcee9a17f16          0.0          0.0          0.0  51.169998              1.0  1010.500000         8.44
+2020-05-14 14:26:06+00:00   24  cfcee9a17f16          0.0          0.0          0.0  51.110001              1.0  1010.500000         8.48
+2020-05-14 14:27:06+00:00   24  cfcee9a17f16         12.0         39.0          4.0  51.189999              1.0  1010.500000         8.51
+2020-05-14 14:28:06+00:00   24  cfcee9a17f16         15.0         23.0          3.0  51.369999              1.0  1010.500000         8.56
+
+[407914 rows x 9 columns]
+```
+
+### CSV
 
 If you give `--outfile filename.csv` argument, the DataFrame
 will be saved into a CSV file. 
-If extension is .xlsx the file format will be excel.
+If the extension is .xlsx the file format will be excel.
 
-## Result
+Result CSV may look like this:
+```
+time,name,dev-id,collision_x,collision_y,collision_z,humidity,motion_detected,pressure,temperature
+2020-04-23 21:00:03+00:00,20,d7356479a2d9,0.0,0.0,0.0,42.45000076293945,0.0,1009.7999877929688,7.119999885559082
+2020-04-23 21:00:06+00:00,25,dc565c713989,0.0,0.0,0.0,45.150001525878906,0.0,1010.0,6.670000076293945
+2020-04-23 21:00:09+00:00,22,f90454752e49,0.0,0.0,0.0,45.72999954223633,0.0,1009.5999755859375,6.96999979019165
+2020-04-23 21:00:37+00:00,07,d60c73a9f7f1,0.0,0.0,0.0,45.40999984741211,0.0,1007.5,4.309999942779541
+2020-04-23 21:00:38+00:00,04,f231f773d177,0.0,0.0,0.0,43.88999938964844,0.0,1006.7999877929688,4.099999904632568
+2020-04-23 21:00:40+00:00,24,cfcee9a17f16,0.0,0.0,0.0,42.9900016784668,0.0,1010.0999755859375,7.170000076293945
+2020-04-23 21:00:40+00:00,02,e1a9bf610eef,0.0,0.0,0.0,42.45000076293945,0.0,1007.0,4.03000020980835
+...
+```
 
-Result DataFrame contains all requested tag data,
-resampled with given time interval. 
+## InfluxDB
 
-``` 
-                             09    10    11    12    13
-time                                                   
-2020-04-25 05:00:00+00:00   8.0  11.0   9.0   7.0   6.0
-2020-04-25 06:00:00+00:00  11.0  14.0  12.0  17.0  10.0
-2020-04-25 07:00:00+00:00  14.0  23.0   8.0  15.0  11.0
-2020-04-25 08:00:00+00:00  20.0  15.0  29.0  36.0  14.0
-2020-04-25 09:00:00+00:00  27.0  21.0  13.0  22.0   6.0
-2020-04-25 10:00:00+00:00  22.0  30.0  29.0  30.0  25.0
-2020-04-25 11:00:00+00:00   9.0  10.0   5.0   3.0   3.0
-2020-04-25 12:00:00+00:00  26.0  24.0  18.0  18.0  14.0
-2020-04-25 13:00:00+00:00  16.0  16.0  12.0  21.0  26.0
-2020-04-25 14:00:00+00:00  22.0  25.0  15.0  23.0  35.0
-2020-04-25 15:00:00+00:00   6.0  11.0  11.0   4.0   6.0
-2020-04-25 16:00:00+00:00   3.0   6.0   4.0   3.0   3.0
-2020-04-25 17:00:00+00:00   9.0  11.0   5.0   5.0  15.0
-2020-04-25 18:00:00+00:00   2.0   7.0   1.0  11.0  12.0
-2020-04-25 19:00:00+00:00   9.0   5.0   5.0   3.0   1.0
-2020-04-25 20:00:00+00:00   0.0   0.0   0.0   0.0   1.0
-2020-04-25 21:00:00+00:00   3.0   2.0   2.0   2.0   2.0
-2020-04-25 22:00:00+00:00   0.0   0.0   0.0   0.0   0.0
-2020-04-25 23:00:00+00:00   0.0   0.0   0.0   0.0   0.0
-2020-04-26 00:00:00+00:00   0.0   0.0   0.0   0.0   0.0
-2020-04-26 01:00:00+00:00   0.0   0.0   0.0   0.0   0.0
-2020-04-26 02:00:00+00:00   0.0   0.0   0.0   0.0   0.0
-2020-04-26 03:00:00+00:00   0.0   2.0   0.0   2.0   1.0
-2020-04-26 04:00:00+00:00   3.0   2.0   2.0   0.0   0.0
-2020-04-26 05:00:00+00:00   6.0   4.0   2.0   4.0   2.0
-2020-04-26 06:00:00+00:00   7.0   9.0   8.0  21.0  11.0
-2020-04-26 07:00:00+00:00  22.0  17.0  24.0  20.0  23.0
-2020-04-26 08:00:00+00:00  40.0  13.0  19.0  25.0  21.0
-2020-04-26 09:00:00+00:00  10.0  24.0  21.0  18.0  22.0
-``` 
+Add required influxdb_* arguments to the script, e.g
+`--influxdb_database` and `--influxdb_measurement`.
--- KaltiotAPI/kaltiot.py
+++ KaltiotAPI/kaltiot.py
@@ -4,94 +4,62 @@ import argparse
 import datetime
 import json
 import logging
-import os
-import sys
+import time
+from urllib.parse import urlencode
 
-import argcomplete
-import dateutil.parser
 import pandas as pd
-import pytz
 import requests
 
-UNITS = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
-MEASUREMENT_CHOISES = ['temperature', 'humidity', 'pressure', 'motion_detected',
+from fvhdms import (
+    epoch2datetime, save_df, get_default_argumentparser, parse_args,
+    user_agent, dataframe_into_influxdb,
+    parse_times
+)
+
+MEASUREMENT_CHOISES = ['motion_detected', 'temperature', 'humidity', 'pressure',
                        'collision_x', 'collision_y', 'collision_z']
-USER_AGENT = 'https://github.com/VekotinVerstas/DataManagementScripts/tree/master/KaltiotAPI kalt.io-client/0.0.1 Python/{}'.format(
-    '.'.join([str(x) for x in list(sys.version_info)[:3]]))
-BASE_URL = 'https://beacontracker.kalt.io/api/history/sensor/'
+USER_AGENT = user_agent('0.1.0', subdir='KaltiotAPI')
 
 
-def datetime_type(value):
-    """Helper for argparse"""
-    if value == 'now':
-        return pytz.UTC.localize(datetime.datetime.utcnow())
-    ts = dateutil.parser.parse(value)
-    if is_naive(ts):
-        raise argparse.ArgumentError('timestamps must have timezone info')
-    return ts
+def parse_tagfile(fname: str) -> list:
+    """Read all tag ids and names from a file.
 
-
-def time_type(value):
-    """Helper for argparse"""
-    return convert_to_seconds(value)
-
-
-def epoch2datetime(epoch):
-    """Convert epoch unix timestamp to a timezone aware datetime (in UTC timezone)
-    :param float epoch: seconds since 1970-01-01T00:00:00Z
-    :return: datetime
+    :param str fname: time period length
+    :return: list of tags [id, name]
     """
-    timestamp = datetime.datetime.utcfromtimestamp(epoch)
-    timestamp = pytz.UTC.localize(timestamp)
-    return timestamp
+    with open(fname, 'rt') as f:
+        taglist = [x.strip().split(',') for x in f.readlines() if not x.startswith('#') and len(x.strip()) >= 14]
+    return taglist
 
 
-def convert_to_seconds(s):
-    """Convert string like 500s, 120m, 24h, 5d, 16w to equivalent number of seconds
-    :param str s: time period length
-    :return: seconds
+def parse_kaltiot_args() -> argparse.Namespace:
+    """Add Kaltiot related arguments into parser.
+
+    :return: result of argparse.parse_args() (argparse.Namespace)
     """
-    return int(s[:-1]) * UNITS[s[-1]]
-
-
-def is_naive(dt):
-    """
-    Check whether a datetime object is timezone aware or not
-    :param Datetime dt: datetime object to check
-    :return: True if dt is naive
-    """
-    if dt.tzinfo is None:
-        return True
-    else:
-        return False
-
-
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--log', dest='log', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                        default='ERROR', help='Set the logging level')
-    parser.add_argument('-st', '--starttime', type=datetime_type,
-                        help='Start time for dump including timezone')
-    parser.add_argument('-et', '--endtime', type=datetime_type, default='now',
-                        help='End time for dump including timezone (default "now")')
-    parser.add_argument('-tl', '--timelength', type=time_type,
-                        help='Length of time for dump [e.g. 500s, 10m, 6h, 5d, 4w]',
-                        default='1d')
+    parser = get_default_argumentparser()
     parser.add_argument('-r', '--resample', help='Pandas resample rule string [e.g. 5min, 1H]', default='5min')
     parser.add_argument('-n', '--names', help='Get only tags with these names [e.g. 04,05,06]')
     parser.add_argument('-t', '--tagfile', required=True, help='File containing list of tag id,name pairs')
     parser.add_argument('-A', '--apikey', required=True, help='Kaltiot API key')
-    parser.add_argument('-m', '--measurement', required=True, choices=MEASUREMENT_CHOISES,
-                        help='File containing list of tag id,name pairs')
-    parser.add_argument('-O', '--outfile', help='Output filename (.xlsx extension creates excel file, others CSV')
-    argcomplete.autocomplete(parser)
-    args = parser.parse_args()
-    if args.log:
-        logging.basicConfig(level=getattr(logging, args.log))
+    parser.add_argument('-B', '--baseurl', required=True, help='Kaltiot API base URL')
+    parser.add_argument('-m', '--measurement', required=True,
+                        help='Download all listed measurements (comma separated), "all" for all')
+    args = parse_args(parser)
     return args
 
 
-def get_data(args, taglist, start_time, end_time):
+def get_data(args: dict, measurement: str, taglist: list,
+             start_time: datetime.datetime, end_time: datetime.datetime):
+    """
+
+    :param dict args:
+    :param str measurement:
+    :param list taglist:
+    :param datetime.datetime start_time:
+    :param datetime.datetime end_time:
+    :return: json object from Kaltiot API OR None
+    """
     if args['names']:
         ids = ','.join([x[0] for x in taglist if x[1] in args['names'].split(',')])
     else:
@@ -105,9 +73,13 @@ def get_data(args, taglist, start_time, end_time):
         'ApiKey': args['apikey'],
         'User-Agent': USER_AGENT,
     }
-    url = f'{BASE_URL}{args["measurement"]}'
+    url = f'{args["baseurl"]}{measurement}'
+    logging.debug('"{}?{}" ApiKey:{}'.format(url, urlencode(params), headers['ApiKey']))
     res = requests.get(url, headers=headers, params=params)
-
+    # Request data is not yet ready, if we got HTTP 206. Check Response status codes here:
+    # https://beacontracker.kalt.io/static/docs/REST-API.html#get-/history/sensor/:sensor_type
+    if res.status_code == 206:
+        return None
     try:
         data = res.json()
         return data
@@ -116,70 +88,144 @@ def get_data(args, taglist, start_time, end_time):
         logging.info(f'Request URL ({res.status_code}): {res.url}')
         logging.info(f'Response text: "{res.text}"')
         logging.info(f'Response headers: "{res.headers}"')
-        exit(1)
+        return None
 
 
-def data_to_dataframe(args, taglist, data):
+def data_to_plaindataframe(measurement: str, taglist: list, data: list) -> pd.DataFrame:
+    """Convert data from Kaltiot API into Pandas DataFrame.
+
+    :param str measurement: Measurement name (temperature, motion_detected etc.)
+    :param list taglist: List of tag ids and names
+    :param list data: Data list from Kaltiot API
+    :return: pd.DataFrame containing all the data
+    """
     dfs = []  # All tag DataFrames go here
+    tagdict = dict(taglist)
     for tag in data:  # Loop all tags
         index = []
         values = []
+        ids = []
+        names = []
         # Extract measurements into index (timestamps) and values lists
         for row in tag['history']:
             index.append(epoch2datetime(row['timestamp'] / 1000))
             values.append(float(row['value']))
+            ids.append(tag['id'])
+            names.append(tagdict[tag['id']])
         # Create a Pandas DataFrame
-        if values:
-            tagdict = dict(taglist)
-            col = tagdict[tag['id']]
-            df = pd.DataFrame(data=values, index=index, columns=[col])
-            df.index.name = 'time'
-            # TODO: Note that resampling e.g. temperature with sum() is usually not very useful
-            # This works well with motion_detected though
-            df = df.resample(args['resample']).sum()
-            df = df.dropna()
-            dfs.append(df)
-    all = pd.concat(dfs, axis=1, sort=False)
-    if args['outfile']:
-        base, ext = os.path.splitext(args['outfile'])
-        if ext == '.xlsx':
-            xall = all.copy()
-            xall['datetime'] = xall.index.to_series().dt.tz_localize(None)
-            xall = xall.reset_index(drop=True)
-            xall.to_excel(args['outfile'])
-        else:
-            all.to_csv(args['outfile'])
-    return all
+        d = {
+            measurement: values,
+            'dev-id': ids,
+            'name': names,
+        }
+        df = pd.DataFrame(data=d, index=index)
+        df.index.name = 'time'
+        dfs.append(df)
+    df_all = pd.concat(dfs, sort=True)
+    return df_all
 
 
-def parse_times(args):
-    # Parse time period's start time
-    if args['starttime']:
-        start_time = args['starttime']
-    else:
-        start_time = args['endtime'] - datetime.timedelta(seconds=args['timelength'])
-    return start_time, args['endtime'], args['timelength']
+def create_requests(maxperiod: int, measurements: list,
+                    start_time: datetime.datetime, end_time: datetime.datetime) -> list:
+    """Create a list of requests which have to be made to get all the data from given time period.
 
-
-def parse_tagfile(fname: str) -> list:
-    """Read all tag ids and names from a file.
-    :param str fname: time period length
-    :return: list of tags
+    :param int maxperiod: maximum time period in seconds
+    :param list measurements: list of measurement names
+    :param datetime.datetime start_time: start timestamp
+    :param datetime.datetime end_time: end timestamp
+    :return: list of http request to make (measurement name, start time, end time)
     """
-    with open(fname, 'rt') as f:
-        taglist = [x.strip().split(',') for x in f.readlines() if not x.startswith('#') and len(x.strip()) >= 14]
-    return taglist
+    timechunks = []
+    chunk_start = start_time
+    chunk_shift = datetime.timedelta(seconds=maxperiod)
+    chunk_end = chunk_start + chunk_shift
+    # Loop until given time period is splitted into chunks of `maxperiod`
+    while chunk_start < end_time:
+        if chunk_end > end_time:
+            chunk_end = end_time
+        timechunks.append([chunk_start, chunk_end])
+        chunk_start += chunk_shift
+        chunk_end += chunk_shift
+    reqs = []
+    for m in measurements:
+        for t in timechunks:
+            reqs.append([m] + t)
+    return reqs
+
+
+def get_multi_data(args: dict, measurement: str, ruuvitaglist: list,
+                   start_time: datetime.datetime, end_time: datetime.datetime) -> pd.DataFrame:
+    """Get data from all measurement endpoints and merge them into one DataFrame.
+
+    :param dict args: command line arguments
+    :param str measurement:
+    :param list ruuvitaglist:
+    :param datetime.datetime start_time:
+    :param datetime.datetime end_time:
+    :return: pd.DataFrame containing all the data from Kaltiot API
+    """
+    df_times = {}  # Dict to save all data per a time period
+    req_attempts = {}  # Book keeping of requests made
+    sleeptime = 1.0
+    if measurement == 'all':
+        measurements = MEASUREMENT_CHOISES
+    else:
+        measurements = measurement.split(',')
+    reqs = create_requests(args.get('maxperiod', 7 * 24 * 60 * 60), measurements, start_time, end_time)
+    req_success = 0
+    req_206 = 0
+    while reqs:
+        r = reqs.pop(0)
+        m, start_time, end_time = r
+        logging.info(f'Processing measurement "{m} ({start_time} --> {end_time}) "')
+        # Count how many times we've tried this request
+        if (m, start_time) not in req_attempts:
+            req_attempts[(m, start_time)] = 1
+        else:  # If previous attempt of this request has failed, add sleep a bit
+            logging.debug(f'Sleep for {sleeptime} seconds')
+            time.sleep(1.0)
+            req_attempts[(m, start_time)] += 1
+        data = get_data(args, m, ruuvitaglist, start_time, end_time)
+        if data is None:  # get_data() got HTTP 206 and we didn't get any data this time
+            req_206 += 1
+            reqs.append(r)  # Add request back for new processing
+            logging.warning(f'Request for "{m}" resulted no data!')
+        else:
+            req_success += 1
+            df = data_to_plaindataframe(m, ruuvitaglist, data)
+            logging.info(df)
+            if start_time not in df_times:
+                df_times[start_time] = df
+            else:  # Merge previous DataFrame to this one
+                df_times[start_time] = pd.merge(df_times[start_time], df,
+                                                on=['time', 'dev-id', 'name'], sort=True)
+    # Concat all time period DataFrames into single one, which contains all the data
+    df_list = []
+    for key in sorted(df_times.keys()):
+        df_list.append(df_times[key])
+    df_all = pd.concat(df_list, sort=True)
+    # Reorder columns (name and dev-id to the beginning)
+    cols = list(df_all.columns)
+    cols_to_move = ['name', 'dev-id']
+    [cols.remove(x) for x in cols_to_move if x in cols]
+    cols = cols_to_move + cols
+    df_all = df_all[cols]
+    logging.info(f'Made {req_success} successful requests and {req_206} HTTP 206 requests')
+    return df_all
 
 
 def main():
-    args = parse_args()
-    args = vars(args)
-    taglist = parse_tagfile(args['tagfile'])
+    # Parse arguments and convert result into a dictionary
+    args = vars(parse_kaltiot_args())
+    # Parse start and end times
     start_time, end_time, time_length = parse_times(args)
-    data = get_data(args, taglist, start_time, end_time)
-    df = data_to_dataframe(args, taglist, data)
-    # Perhaps you'd like to do some data analysis here instead of printing the result?
+    # Parse RuuviTag list (ids, names)
+    ruuvitaglist = parse_tagfile(args['tagfile'])
+    # Get data for all measurements
+    df = get_multi_data(args, args['measurement'], ruuvitaglist, start_time, end_time)
     print(df)
+    save_df(args, df)
+    dataframe_into_influxdb(args, df, tag_columns=['dev-id', 'name'])
 
 
 if __name__ == '__main__':
