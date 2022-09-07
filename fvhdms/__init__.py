d2e026e274dba6d86d5a669284bf67b49f41b280
Aapo (f021b) Rista <aapris@gmail.com>
2022-09-07 17:58:37 +0300

    Add --sentry-dns arg

--- fvhdms/__init__.py
+++ fvhdms/__init__.py
@@ -12,16 +12,17 @@ import dateutil.parser
 import pandas as pd
 import pytz
 from influxdb import DataFrameClient
+from influxdb.exceptions import InfluxDBClientError
 
 UNITS = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
 
 
 def user_agent(version: str, subdir=None) -> str:
     """Create uniform User-Agent string for requests"""
-    ua = 'https://github.com/VekotinVerstas/DataManagementScripts'
+    ua = "https://github.com/VekotinVerstas/DataManagementScripts"
     if subdir is not None:
-        ua = f'{ua}/tree/master/{subdir}'
-    ua += ' api-client/{} Python/{}'.format(version, '.'.join([str(x) for x in list(sys.version_info)[:3]]))
+        ua = f"{ua}/tree/master/{subdir}"
+    ua += " api-client/{} Python/{}".format(version, ".".join([str(x) for x in list(sys.version_info)[:3]]))
     return ua
 
 
@@ -53,11 +54,11 @@ def datetime_type(value: str) -> datetime.datetime:
     Check that given argument is parsable by dateutil.parser.parse()
     and return timezone aware datetime.
     """
-    if value == 'now':
+    if value == "now":
         return pytz.UTC.localize(datetime.datetime.utcnow())
     ts = dateutil.parser.parse(value)
     if is_naive(ts):
-        raise argparse.ArgumentError('timestamps must have timezone info')
+        raise argparse.ArgumentError("timestamps must have timezone info")
     return ts
 
 
@@ -86,16 +87,16 @@ def epoch2datetime(epoch: int) -> datetime.datetime:
 
 def save_df(args: dict, df: pd.DataFrame) -> bool:
     """Save Pandas DataFrame to a file in excel or CSV format (depending on extension)"""
-    if args.get('outfile') is not None:
-        base, ext = os.path.splitext(args['outfile'])
-        if ext == '.xlsx':
-            df['datetime'] = df.index.to_series().dt.tz_localize(None)
+    if args.get("outfile") is not None:
+        base, ext = os.path.splitext(args["outfile"])
+        if ext == ".xlsx":
+            df["datetime"] = df.index.to_series().dt.tz_localize(None)
             df = df.reset_index(drop=True)
-            df.to_excel(args['outfile'])
-            logging.info('Saved dataframe to excel file {}'.format(args['outfile']))
+            df.to_excel(args["outfile"])
+            logging.info("Saved dataframe to excel file {}".format(args["outfile"]))
         else:
-            df.to_csv(args['outfile'])
-            logging.info('Saved dataframe to CSV file {}'.format(args['outfile']))
+            df.to_csv(args["outfile"])
+            logging.info("Saved dataframe to CSV file {}".format(args["outfile"]))
         return True
     else:
         return False
@@ -110,25 +111,39 @@ def get_default_argumentparser() -> argparse.ArgumentParser:
     :return: argparse.ArgumentParser
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--log', dest='log', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                        default='ERROR', help='Set the logging level')
-    parser.add_argument('-st', '--starttime', type=datetime_type,
-                        help='Start time for dump including timezone')
-    parser.add_argument('-et', '--endtime', type=datetime_type, default='now',
-                        help='End time for dump including timezone (default "now")')
-    parser.add_argument('-tl', '--timelength', type=time_type,
-                        help='Length of time for dump [e.g. 500s, 10m, 6h, 5d, 4w]',
-                        default='1d')
-    parser.add_argument('-mp', '--maxperiod', type=time_type,
-                        help='Maximum time period chunk size [e.g. 500s, 10m, 6h, 5d, 4w]',
-                        default='1d')
-    parser.add_argument('-iD', '--influxdb_database', help='InfluxDB database name')
-    parser.add_argument('-im', '--influxdb_measurement', help='InfluxDB measurement name')
-    parser.add_argument('-ih', '--influxdb_host', default='127.0.0.1', help='InfluxDB host')
-    parser.add_argument('-ip', '--influxdb_port', default=8086, help='InfluxDB port')
-    parser.add_argument('-iu', '--influxdb_username', help='InfluxDB username')
-    parser.add_argument('-iP', '--influxdb_password', help='InfluxDB password')
-    parser.add_argument('--outfile', help='Output filename (.xlsx extension creates excel file, others CSV')
+    parser.add_argument(
+        "--log",
+        dest="log",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        default="ERROR",
+        help="Set the logging level",
+    )
+    parser.add_argument("-st", "--starttime", type=datetime_type, help="Start time for dump including timezone")
+    parser.add_argument(
+        "-et",
+        "--endtime",
+        type=datetime_type,
+        default="now",
+        help='End time for dump including timezone (default "now")',
+    )
+    parser.add_argument(
+        "-tl", "--timelength", type=time_type, help="Length of time for dump [e.g. 500s, 10m, 6h, 5d, 4w]", default="1d"
+    )
+    parser.add_argument(
+        "-mp",
+        "--maxperiod",
+        type=time_type,
+        help="Maximum time period chunk size [e.g. 500s, 10m, 6h, 5d, 4w]",
+        default="1d",
+    )
+    parser.add_argument("-iD", "--influxdb_database", help="InfluxDB database name")
+    parser.add_argument("-im", "--influxdb_measurement", help="InfluxDB measurement name")
+    parser.add_argument("-ih", "--influxdb_host", default="127.0.0.1", help="InfluxDB host")
+    parser.add_argument("-ip", "--influxdb_port", default=8086, help="InfluxDB port")
+    parser.add_argument("-iu", "--influxdb_username", help="InfluxDB username")
+    parser.add_argument("-iP", "--influxdb_password", help="InfluxDB password")
+    parser.add_argument("--outfile", help="Output filename (.xlsx extension creates excel file, others CSV")
+    parser.add_argument("--sentry-dns", required=False, help="sentry_dns uri, if Sentry is in use")
     return parser
 
 
@@ -141,33 +156,52 @@ def parse_args(parser) -> argparse.Namespace:
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
     if args.log:
-        logging.basicConfig(level=getattr(logging, args.log), datefmt='%Y-%m-%dT%H:%M:%S',
-                            format="%(asctime)s.%(msecs)03dZ %(levelname)s %(message)s")
+        logging.basicConfig(
+            level=getattr(logging, args.log),
+            datefmt="%Y-%m-%dT%H:%M:%S",
+            format="%(asctime)s.%(msecs)03dZ %(levelname)s %(message)s",
+        )
         logging.Formatter.converter = time.gmtime  # Timestamps in UTC time
     return args
 
 
 def parse_times(args: dict) -> (datetime.datetime, datetime.datetime, int):
     """Parse time period's start time"""
-    if args['starttime']:
-        start_time = args['starttime']
+    if args["starttime"]:
+        start_time = args["starttime"]
     else:
-        start_time = args['endtime'] - datetime.timedelta(seconds=args['timelength'])
-    return start_time, args['endtime'], args['timelength']
+        start_time = args["endtime"] - datetime.timedelta(seconds=args["timelength"])
+    return start_time, args["endtime"], args["timelength"]
 
 
 def dataframe_into_influxdb(args: dict, df: pd.DataFrame, tag_columns=None):
     if tag_columns is None:
-        tag_columns = ['dev-id']
-    if args.get('influxdb_database') is None or args.get('influxdb_measurement') is None:
-        logging.debug('Not saving into InfluxDB (no database or measurement name given')
+        tag_columns = ["dev-id"]
+    if args.get("influxdb_database") is None or args.get("influxdb_measurement") is None:
+        logging.debug("Not saving into InfluxDB (no database or measurement name given")
         return False
-    protocol = 'line'
-    client = DataFrameClient(host=args.get('influxdb_host'), port=args.get('influxdb_port'),
-                             username=args.get('influxdb_username'), password=args.get('influxdb_password'),
-                             database=args.get('influxdb_database'))
-    logging.info('Create database: {}'.format(args.get('influxdb_database')))
-    client.create_database(args.get('influxdb_database'))
-    client.write_points(df, args.get('influxdb_measurement'),
-                        tag_columns=tag_columns, protocol=protocol, batch_size=5000)
+    protocol = "line"
+    client = DataFrameClient(
+        host=args.get("influxdb_host"),
+        port=args.get("influxdb_port"),
+        username=args.get("influxdb_username"),
+        password=args.get("influxdb_password"),
+        database=args.get("influxdb_database"),
+    )
+    logging.info("Create database: {}".format(args.get("influxdb_database")))
+    client.create_database(args.get("influxdb_database"))
+    len1 = len(df)
+    df = df.dropna(thresh=2)  # Drop all lines where all but dev-id columns are NaN
+    len2 = len(df)
+    if len1 > len2:
+        logging.warning("Dropped {} NaN rows".format(len1 - len2))
+    try:
+        client.write_points(
+            df, args.get("influxdb_measurement"), tag_columns=tag_columns, protocol=protocol, batch_size=5000
+        )
+    except InfluxDBClientError:
+        df.to_csv("error_data.csv")
+        logging.error("Erroneous DF written to file error_data.csv")
+        raise
+
     return True
